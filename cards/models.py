c6bf1cbc7d18dc46a55d6936d8f3c5e35ecd17a5
Laura Macaluso <lauramacaluso@Lauras-MBP.fios-router.home>
2018-12-07 20:15:21 -0500

    requirements and adding some simple models and views

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,13 @@
+import os
 from django.db import models
 
+
 # Create your models here.
+def get_image_path(instance, filename):
+    return os.path.join('photos', str(instance.id), filename)
+
+
+class Card(models.Model):
+    identifier = models.CharField(max_length=30)
+    date_added = models.DateTimeField('Date Added')
+    picture = models.ImageField(upload_to=get_image_path, blank=True, null=True)
